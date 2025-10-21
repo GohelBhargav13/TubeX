6e2f312d66c98a8d42a15f51adfab62007c88b96
= <gohelbhargav401@gmail.com>
2025-10-22 00:36:05 +0530

    Mail Service done

---
+++ backend/src/utills/constant.js
@@ -0,0 +1,11 @@
+const UserRole = {
+    USER:"user",
+    ADMIN:"admin"
+}
+
+// String Array of the User Roles
+const AvailableUserRole = Object.values(UserRole);
+
+
+
+export { UserRole,AvailableUserRole }
---
+++ backend/src/utills/mail.js
@@ -0,0 +1,70 @@
+import nodemailer from "nodemailer"
+import mailgen from "mailgen"
+
+export const sendEmail = async(options) => {
+    const mailGenerator = new mailgen({
+    theme: 'default',
+    product: {
+        // Appears in header & footer of e-mails
+        name: 'Mailgen',
+        link: 'https://mailgen.js/'
+        // Optional product logo
+        // logo: 'https://mailgen.js/img/logo.png'
+    }
+});
+
+const textMail = mailGenerator.generatePlaintext(options.mailgenContent);
+const htmlMail = mailGenerator.generate(options.mailgenContent);
+
+
+// Send the mail Using Nodemailer
+
+let transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOSTNAME,
+    port: process.env.MAIL_PORT,
+    secure: false,
+    auth: {
+        user: process.env.SMTP_EMAIL,
+        pass: process.env.SMTP_PASSWORD
+    }
+});
+
+const MailOptions = {
+    from:process.env.SMTP_EMAIL,
+    to:options.email,
+    subject:options.subject,
+    text:textMail,
+    html:htmlMail
+}
+
+try {
+
+    await transporter.sendMail(MailOptions)
+    console.log("Mail Sent.....")
+    
+} catch (error) {
+    console.log("Error in Mail Sending")
+}
+
+}
+
+export const verificationEmailTemplate = (username, verifyURL) => {
+  return {
+    body: {
+      name: `${username} !`,
+      intro:
+        "Thanks for signing up for Mailgen. We're very excited to have you on board.",
+      action: {
+        instructions:
+          "To get started using Mailgen, please confirm your account below:",
+        button: {
+          color: "#22BC66", // Green like in screenshot
+          text: "Confirm your account",
+          link: `${process.env.FRONTEND_URL}/verify-email/${verifyURL}`,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
