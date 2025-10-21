34dbf1c7662d9933a3b82fe4900069ed927765c6
= <gohelbhargav401@gmail.com>
2025-10-21 23:12:36 +0530

    Video and User controller written

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,64 @@
+import User from "../models/user.models.js";
+import ApiError from "../utills/api-error.js";
+import ApiResponse from "../utills/api-response.js";
+
+export const registerUser = async (req, res) => {
+  console.log(req.body);
+  const { userEmail, userFirstName,userLastName, user_avatar, userPassword } = req.body;
+  
+  try {
+    const existingUser = await User.findOne({ userEmail });
+    if (existingUser) {
+      return res.status(400).json(new ApiError(400, "User already exists"));
+    }
+
+    //check for the user_avatar is upload to the localpath
+    console.log(req.file);
+    const user_avatar_upload = req.file?.path;
+
+    if(!user_avatar_upload){
+        return res.status(400).json(new ApiError(400,"Image is not uploaded"))
+    }
+
+    const cloudPathImage = await uploadImageOnCloudinary(user_avatar_upload);
+    if(!cloudPathImage){
+        return res.status(400).json(new ApiError(400,"File is not uploaded in cloud"))
+    }
+
+    const newUser = await User.create({
+      userEmail,
+      userFirstName,
+      userLastName,
+      user_avatar:cloudPathImage,
+      userPassword,
+    });
+
+    //Generate email verification token
+    const emailToken = newUser.generateEmailVerifiactionToken();
+
+    await newUser.save();
+
+    //Send verification email
+    await sendEmail({
+      email: newUser.email,
+      subject: "Verification Email",
+      mailgencontent: verificationEmailTemplate(name, emailToken.unhashedToken),
+    });
+
+    res
+      .status(201)
+      .json(
+        new ApiResponse(
+          201,
+          { id: newUser._id, email: newUser.email,user_avatar:cloudPathImage },
+          "User registerd sucessfully please Verify Your Email"
+        )
+      );
+  } catch (error) {
+    res
+      .status(500)
+      .json(
+        new ApiError(500, "Internal Error in Registering User", error.message)
+      );
+  }
+};
--- backend/src/controllers/video.controllers.js
+++ backend/src/controllers/video.controllers.js
@@ -1,15 +1,20 @@
 import ffmpeg from "fluent-ffmpeg";
 import pathVal from "path";
 import { uploadVideoToS3 } from "../utills/s3.configuration.js";
+import Video from "../models/video.models.js";
+import ApiResponse from "../utills/api-response.js"
+import ApiError from "../utills/api-error.js"
+import mongoose from "mongoose";
 
 export const uploadVideo = async (req, res) => {
+
+  const { videoTitle,videoDescription } = req.body;
+
   console.log("This is the video files : ", req.file);
 
   // take the path of the input video file
   const { path } = req.file;
-  const output_fileName = `${pathVal.join(process.cwd(), "Output")}/${
-    req.file.originalname.split(".")[0]
-  }.m3u8`;
+  const output_fileName = `${pathVal.join(process.cwd(), "Output")}/${req.file.originalname.split(".")[0]}.m3u8`;
   const upload_clodinary_path = `${pathVal.join(process.cwd(), "Output")}/`;
 
   try {
@@ -17,7 +22,7 @@ export const uploadVideo = async (req, res) => {
       ffmpeg(path)
         .outputOptions([
           "-start_number 0",
-          "-hls_time 5", // each chunk 10 sec
+          "-hls_time 10", // each chunk 10 sec
           "-hls_list_size 0",
           "-f hls",
         ])
@@ -33,22 +38,80 @@ export const uploadVideo = async (req, res) => {
         .run();
     });
 
-    // Function for the clodinary data upload
+    // Function for the S3 data upload
     const url_data = await uploadVideoToS3(upload_clodinary_path);
     // Print the data
-    console.log("Data From the cloudinary : ", url_data);
+    console.log("Data From the S3 : ", url_data);
 
-    res
-      .status(200)
-      .json({
-        message: "Video is Converted Successfully",
-        success: true,
-        data: url_data,
-      });
+    // add details of the video in database
+
+    // Check the data is available or not
+    if(!videoTitle || !videoDescription || url_data.length === 0){
+      return res.status(404).json(new ApiError(404,"Video Details Are Required"))
+    }
+
+    // Create the new document in the database
+      const newVideo = await Video.create({
+        videoTitle,
+        videoDescription,
+        videoUrl: url_data[0],
+        videoOwner:new mongoose.Types.ObjectId(123456789)
+      })
+
+      if(!newVideo){
+        return res.status(404).json(new ApiError(404,"Error in New Video Creation"))
+      }
+
+      console.log("New Video is : ",newVideo)
+
+      // Sending a response of the new Video
+      res.status(201).json(new ApiResponse(201,newVideo,"Video is uploaded successfully"))
+
   } catch (error) {
     console.log("Catch part Error : ", error);
-    res
-      .status(500)
-      .json({ message: "Video is not uploaded From catch", success: false });
+    res.status(500).json(new ApiError(500,"Internal Server Error In Upload Video"))
   }
 };
+
+// getting all video for the home
+export const getVideos = async (req,res) => {
+  try {
+
+    const videos = await Video.find().select("-__v -postdAt -updatedAt")
+
+    if(videos.length === 0){
+      return res.status(400).json(new ApiError(400,"No Videos Are Available"))
+    }
+
+    res.status(200).json(new ApiResponse(200,videos,"Videos are fetched successfully"))
+    
+  } catch (error) {
+    console.log("Error in fetching all videos : ",error);
+    res.status(500).json(new ApiError(500,"Internal Error in fetching videos"))
+  }
+}
+
+// get video by ID
+export const getVideoId = async(req,res) => {
+  const { videoId } = req.params;
+  try {
+
+    if(!videoId){
+      return res.status(404).json(new ApiError(404,"Video Id is not found"))
+    }
+
+    const video = await Video.findById(videoId)
+    .select("-__v -postdAt -updatedAt")
+
+    if(!video){
+      return res.status(400).json(new ApiError(400,"Video is Not Found Yet"))
+    }
+
+    res.status(200).json(new ApiResponse(200,video,"Video is fetched Successfully"))
+    
+  } catch (error) {
+    console.log("Error in fetch the video by ID : ",error)
+    res.status(500).json(new ApiError(500,"Internal Error in fetching video by ID",err))
+  }
+
+} 
