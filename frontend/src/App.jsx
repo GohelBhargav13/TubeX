1a996c2b2583b8ca9a4e2ed06bed9ab7f648604e
= <gohelbhargav401@gmail.com>
2025-10-24 12:51:56 +0530

    Tailwind css configuration completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,19 @@
-import { useState } from "react";
-import axios from "axios";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage.jsx";
+import ProtectedRoute from "./utills/ProtectedRoute.jsx";
+import HomePage from "./pages/HomePage.jsx";
+
 
 function App() {
-  const [video, setVideo] = useState(null);
-  const [finalmessage, setFinalMessage] = useState("");
-
-  const handleVideoUpload = async () => {
-    const formData = new FormData();
-
-    formData.append("video", video);
-    console.log(formData);
-
-   try {
-     const res = await axios.post(
-       "http://localhost:4000/video/upload-videos",
-       formData,
-       {
-         headers: {
-           "Content-Type": "multipart/form-data",
-         },
-       }
-     );
- 
-     console.log(res.data);
-     if (res.data.success) {
-       setFinalMessage(res.data.message);
-       setVideo(null)
-     } else {
-       setFinalMessage(res.data.message || "video is not going to backend");
-     }
-   } catch (error) {
-    console.log(error)
-   }
-  };
-
+  
   return (
-    <>
-      {finalmessage && (
-        <span style={{ color: "black", fontWeight: "bolder" }}>
-          {finalmessage}
-        </span>
-      )}
-      <input
-        type="file"
-        name="video"
-        accept="video/*"
-        onChange={(e) => setVideo(e.target.files[0])}
-      />
-      <button onClick={handleVideoUpload} disabled={!video} >CLick</button>
-    </>
+
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/home" element={<ProtectedRoute> <HomePage /> </ProtectedRoute>} />
+      {/* <Route path="*" element={ <Navigatex` to="/login" /> } /> */}
+    </Routes>
   );
 }
 
