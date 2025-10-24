00a8cf29bac7687c758a43bf19a348396bf3974c
= <gohelbhargav401@gmail.com>
2025-10-25 00:31:12 +0530

    server file updated

--- backend/src/Server/Server.js
+++ backend/src/Server/Server.js
@@ -1,10 +1,11 @@
 import { Server } from "socket.io"
 import app from "../../app.js"
 import http from "http"
+import { likeVideo } from "../API/video.api.js"
 
 export const server = http.createServer(app)
 
-const io = new Server(server,{
+export const io = new Server(server,{
     cors:{
         origin:["http://localhost:5173","http://localhost:5174"],
         credentials:true,
@@ -15,6 +16,11 @@ const io = new Server(server,{
 
 io.on("connection",(socket) => {
     console.log("New Socket Connection : ",socket.id);
+
+    socket.on("likePost", async ({ videoId,userId }) => {
+        // console.log("Receving Like Event : ",videoId,userId)
+        await likeVideo(videoId,userId,socket)
+    })
 
 
     socket.on("disconnect",() => {
