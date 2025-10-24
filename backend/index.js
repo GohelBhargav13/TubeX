d857e4aeaf719cd50d04899d398d9385b0792049
= <gohelbhargav401@gmail.com>
2025-10-25 00:31:47 +0530

    Index.js updated

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 
 import { db } from "./src/db-config/db.js";
 import app from "./app.js";
+import { server } from "./src/Server/Server.js"
 
 const PORT = process.env.PORT || 4000;
 
 // IF DB is connect then start the server
 db()
   .then(() => {
-    app.listen(PORT, () => console.log(`Server is Running on port ${PORT}`));
+    server.listen(PORT, () => console.log(`Server is Running on port ${PORT}`));
   })
   .catch((error) => console.log(error));
