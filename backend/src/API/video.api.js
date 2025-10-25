4296745760c9cad5243156d1dcb18dcbc6bcfad5
= <gohelbhargav401@gmail.com>
2025-10-25 20:29:49 +0530

    Video.api.js file updated

--- backend/src/API/video.api.js
+++ backend/src/API/video.api.js
@@ -65,3 +65,59 @@ export const likeVideo = async (videoId, userId, socket) => {
     return;
   }
 };
+
+export const commentVideo = async (comment,userId,videoId,socket) => {
+   try {
+    if (!videoId || !userId || !comment) {
+       socket.emit("ErrorInSocket", {
+        message: "All Parameters are Required",
+        success: false,
+      });
+      return;
+    }
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+       socket.emit("ErrorInSocket", {
+        message: "Video Is not Found",
+        success: false,
+      });
+      return;
+    }
+
+    const user = await Userm.findById(userId);
+    if (!user) {
+       socket.emit("ErrorInSocket", {
+        message: "User is not found",
+        success: false,
+      });
+      return;
+    }
+
+    // Add the Comment of the user
+    video.videoComments.push({ user:userId,comment })
+    await video.save()
+
+    const userInfo = await video.populate("videoComments.user","userFirstName userLastName user_avatar");
+    // event emit for the comment on the video
+
+    io.emit("VideoCommentUpdated", {
+      New_Comment:video.videoComments[video.videoComments.length - 1],
+      commentCount:video.videoComments.length,
+      userId,
+      videoId,
+      message:"Video Commented Successfully",
+      commentId:video.videoComments[video.videoComments.length - 1]._id
+    })
+   
+
+  } catch (error) {
+    console.log("Error in Commenting the video : ", error);
+     socket.emit("ErrorInSocket", {
+        message: "Error from catch part in commenting the video",
+        success: false,
+      });
+      return;
+  }
+}
