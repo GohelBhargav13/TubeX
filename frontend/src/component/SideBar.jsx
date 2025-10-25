0a4aaf4a051284d9637ef4f9795aefc5f5a595bf
= <gohelbhargav401@gmail.com>
2025-10-25 23:02:49 +0530

    Siderbar update with dynamic state change

--- frontend/src/component/SideBar.jsx
+++ frontend/src/component/SideBar.jsx
@@ -1,5 +1,5 @@
-
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import { useUserAuthStore } from "../store/auth.store.js";
 
 // Routes to different Pages
 const Routes = [
@@ -10,8 +10,8 @@ const Routes = [
 ];
 
 const SideBar = () => {
-
   const navigate = useNavigate();
+  const { userData, userLogout } = useUserAuthStore();
 
   // handle a navigation
   const navigationTo = (path) => {
@@ -19,24 +19,30 @@ const SideBar = () => {
   };
 
   return (
-   <>
-     {/* Side Panel */}
+    <>
+      {/* Side Panel */}
       <div className="w-60 bg-white shadow-md p-4 flex flex-col space-y-4">
         <h2 className="text-2xl font-bold text-blue-600">TubeX</h2>
         <nav className="flex flex-col space-y-2 font-bold">
-          {Routes.map((route) => (
-            <button
+          {Routes.map((route) =>
+            route.name === "Login" && userData ? (
+              <button
+                type="submit"
+                className="text-left px-3 py-2 hover:bg-gray-200 rounded"
+                onClick={() => userLogout()}
+              > Logout</button>
+            ) : (
+              <button
               type="submit"
               className="text-left px-3 py-2 hover:bg-gray-200 rounded"
               onClick={() => navigationTo(route.path)}
-            >
-              {route.name}
-            </button>
-          ))}
+              >{ route.name }</button>
+            )
+          )}
         </nav>
       </div>
-   </>
-  )
-}
+    </>
+  );
+};
 
-export default SideBar
+export default SideBar;
