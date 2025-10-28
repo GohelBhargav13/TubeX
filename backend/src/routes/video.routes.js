e519672704b19d54fcef90faedab80df7470380e
= <gohelbhargav401@gmail.com>
2025-10-29 00:59:00 +0530

    video routes are updated

--- backend/src/routes/video.routes.js
+++ backend/src/routes/video.routes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { upload } from "../multer-config/multer.js"
-import { deleteComment, deleteVideo, getVideoId, getVideos, uploadVideo, videoComment, videoLiked } from "../controllers/video.controllers.js";
+import { deleteComment, deleteVideo, getVideoId, getVideos, updateVideo, uploadVideo, videoComment, videoLiked } from "../controllers/video.controllers.js";
 import IsLoggedIn from "../middleware/auth.middleware.js";
 import { checkUserRole } from "../middleware/check.middleware.js";
 import {  UserRole } from "../utills/constant.js";
@@ -14,6 +14,7 @@ videoRoutes.get("/get-video/:videoId",IsLoggedIn,getVideoId)
 videoRoutes.post("/video-like/:videoId",IsLoggedIn,videoLiked)
 videoRoutes.post("/video-comment/:videoId",IsLoggedIn,videoComment)
 videoRoutes.delete("/video-comment-delete/:videoId",IsLoggedIn,deleteComment)
-videoRoutes.delete("/delete-videos/:videoId",IsLoggedIn,checkUserRole([UserRole.ADMIN]),deleteVideo)
+videoRoutes.delete("/delete-video/:videoId",IsLoggedIn,checkUserRole([UserRole.ADMIN]),deleteVideo)
+videoRoutes.post("/update-video/:videoId",IsLoggedIn,updateVideo)
 
 export default videoRoutes
