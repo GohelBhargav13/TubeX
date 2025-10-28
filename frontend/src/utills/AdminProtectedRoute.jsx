f47d0d5d17281e46abebc8cd990a03019ece3fe1
= <gohelbhargav401@gmail.com>
2025-10-29 00:52:30 +0530

    New Protected Route is added

--- frontend/src/utills/AdminProtectedRoute.jsx
+++ frontend/src/utills/AdminProtectedRoute.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
+import { useUserAuthStore } from "../store/auth.store.js"
 
-const AdminProtectedRoute = ({ user,children }) => {
+const AdminProtectedRoute = ({ children }) => {
 
-  if(!user) return <Navigate to="/login" />
-  return user.userRole === "admin" ? children : <Navigate to="/home" />
+  const { userData,isFetching,isChecked  } = useUserAuthStore()
+
+  if(isFetching || !isChecked) return <h1>Loading...</h1>
+
+  if(!userData) return <Navigate to="/login" />
+  return userData?.userRole === "admin" ? children : <Navigate to="/home" />
 }
 
-export default AdminProtectedRoute
+export default AdminProtectedRoute  
