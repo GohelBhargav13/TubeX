943c6bb59afe0f26b3520a4307c5b95b04cdba5a
= <gohelbhargav401@gmail.com>
2025-10-30 21:17:33 +0530

    user routes is updated

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { getMe, getUserLikedVideos, getUserVideos, registerUser, userLogin, userLogout, verifyEmail } from "../controllers/user.controller.js";
+import { chanegUserRole, getAllUsers, getMe, getUserLikedVideos, getUserVideos, registerUser, userLogin, userLogout, verifyEmail } from "../controllers/user.controller.js";
 import { upload } from "../multer-config/multer_user_avatar.js";
 import IsLoggedIn from "../middleware/auth.middleware.js";
 
@@ -12,6 +12,8 @@ userRoutes.post("/login",userLogin)
 userRoutes.get("/me",IsLoggedIn,getMe)
 userRoutes.get("/get-liked-videos",IsLoggedIn,getUserLikedVideos)
 userRoutes.get("/user-videos",IsLoggedIn,getUserVideos)
+userRoutes.get("/all-users",IsLoggedIn,getAllUsers)
+userRoutes.post("/change-userRole/:userId",IsLoggedIn,chanegUserRole)
 userRoutes.get("/logout",IsLoggedIn,userLogout)
 
 export default userRoutes
