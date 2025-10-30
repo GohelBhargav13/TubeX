f1aa6ab0a26d7cad1995f8884e658ff99f4e2773
= <gohelbhargav401@gmail.com>
2025-10-30 21:21:20 +0530

    Api file fixed

--- frontend/src/API/user.api.js
+++ frontend/src/API/user.api.js
@@ -23,14 +23,16 @@ export  const fetchAllUsers = async () => {
 export const changeUsersRole = async (userId,userRole) => {
     try {
         console.log("UserID in API : ", userId)
-        if(userId) return;
+        console.log("UserRole in API : ", userRole)
+        if(!userId) return;
 
         const data = {
             userRole:userRole
         }
 
        const res = await api.post(`/user/change-userRole/${userId}`,data);
-       console.log("Response after change a UserRole Backend: ", res)
+       console.log("Response after change a UserRole Backend: ", res?.data)
+
        if(res?.data?.StatusCode === 200){
             console.log("User role Changed : ", res?.data?.data)
             return { data:res?.data?.data, success:true, updatedRole:res?.data?.UpdatedRole, message:res?.data?.message }
