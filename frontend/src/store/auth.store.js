61c4196bd758f4984947b10353b9a5a126cfd070
= <gohelbhargav401@gmail.com>
2025-11-04 16:50:26 +0530

    Auth Store is Updated

--- frontend/src/store/auth.store.js
+++ frontend/src/store/auth.store.js
@@ -2,40 +2,104 @@ import { create } from "zustand";
 import { api } from "../services/axios.js";
 import { toast } from "react-hot-toast";
 
-export const useUserAuthStore = create((set) => ({
+export const useUserAuthStore = create((set,get) => ({
   userData: null,
   isFetching: false,
   isChecked:false,
 
+  // Login Module 
+  userLogin: async (formData) => {
+
+    const { setUserData } = get()
+
+    try {
+        set({ isFetching: true })
+
+        if(get().userData){
+          console.log("User is Already logged In")
+          set({ isChecked: true })
+          return { status:true };
+        }
+
+        // Make a login request to the backend
+       const res = await api.post("/user/login", formData,{ withCredentials:true })
+       console.log(res?.data)
+
+       const message = res?.data?.Message;
+       const StatusCode = res?.data?.StatusCode;
+
+       if(StatusCode === 200 || StatusCode === 201){
+          console.log("User data is set.....");
+          await setUserData()
+          toast.success("Login Successfull")
+          return { status:true };
+       }
+      
+       if(StatusCode === 404){
+        toast.error(message || "Something is not found")
+        return { status:false };
+       }
+
+       if(StatusCode === 401){
+          toast.error(message || "Something Error from backend error")
+          return { status:false };
+       }
+
+      
+    } catch (error) {
+        set({ userData:null })
+        console.log("Error While login the user")
+        toast.error(error?.response?.data?.message || "Login failed due to server error");
+        return { status: false };
+  
+    }finally {
+      set({ isFetching:false })
+    }
+  },
+
+  // User Data Fetch
   setUserData: async () => {
     try {
       set({ isFetching: true });
 
-      const res = await api.get("/user/me");
-
-      // Safe access + toast
-      const user = res?.data?.data || null;
-      // console.log(user);
-      
-      set({ userData: user,isChecked:true });
-
-      if (user) {
-        // return toast.success(res?.data?.message || "Data fetched successfully");
-          console.log("User Data is already Fetched....")
-      } else {
-        toast.error("Failed to fetch user data");
+      // check if the user data is already exist than return
+      if(get().userData) {
+        set({ isChecked:true })
+        return;
       }
 
+      const res = await api.get("/user/me");
+
+      const StatusCode = res?.data?.StatusCode;
+      const message = res?.data?.Message;
+
+      if(StatusCode === 401){
+          toast.error(message || "Please Fill the fields")
+          return;
+      }
+
+        // Safe access + toast
+      if(StatusCode === 200 || StatusCode === 201){
+         const user = res?.data?.data || null;      
+         set({ userData: user,isChecked:true });
+
+          if (user !== null) {
+              console.log("User Data is already Fetched....")
+          } else {
+            toast.error("Failed to fetch user data");
+          }
+
+      }
     } catch (error) {
       console.error("Error in fetching user data:", error);
-      set({ userData: null ,isChecked:true});
-      toast.error("Error fetching user data");
-
+        set({ userData: null ,isChecked:true});
+        // toast.error("Error fetching user data");
     } finally {
       set({ isFetching: false });
     }
   },
 
+  // User LogOut
   userLogout: async () => {
     try {
 
