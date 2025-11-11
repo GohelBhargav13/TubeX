8eca3f3f5571ba49c759081f6bb23a040e73c12e
= <gohelbhargav401@gmail.com>
2025-11-11 13:41:10 +0530

    Server.js is updated

--- backend/src/Server/Server.js
+++ backend/src/Server/Server.js
@@ -8,7 +8,7 @@ export const server = http.createServer(app)
 
 export const io = new Server(server,{
     cors:{
-        origin:["http://localhost:5173","http://localhost:5174",'http://13.51.106.105:5173'],
+        origin:["http://localhost:5173","http://localhost:5174",'http://13.51.106.105:5174'],
         credentials:true,
         methods:["GET","POST","DELETE","OPTION","PUT","PATCH"]
     }
