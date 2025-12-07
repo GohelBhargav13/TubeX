84eaa92c815e5fc93dc62ecbe88ee6de7ebc3ea6
= <gohelbhargav401@gmail.com>
2025-12-07 18:04:07 +0530

    Fix: multer file is updated

--- backend/src/multer-config/multer.js
+++ backend/src/multer-config/multer.js
@@ -1,9 +1,14 @@
 import multer from "multer"
 import path from "path"
+import fs from "node:fs"
 
 const storage = multer.diskStorage({
     // Define the destination folder for the video store
     destination:function(req,file,cb){
+        if(!fs.existsSync(path.join(process.cwd(),"Videos"))){
+            console.log("videos folder created at :", path.join(process.cwd(),"Videos"))
+            fs.mkdirSync(path.join(process.cwd(),"Videos"))
+        }
         cb(null,path.join(process.cwd(),"Videos"))
     },
     // for the function to store the file on the destination folder 
